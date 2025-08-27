6cfdee2e06e65ce60eb3331bb12e4bc01cc30dff
Prateek Dwivedi <prateekdwivedi@Prateeks-MacBook-Air.local>
2025-08-27 13:47:01 +0530

    Fix: Corrected API route path

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getProducts, getProductById } = require('../controllers/productController.js');
 
-// Route for getting all products
 router.route('/').get(getProducts);
-
-// Route for getting a single product by its id
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById); // <-- Check this line specifically
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, authUser } = require('../controllers/userController.js');
 
-// Route for registering a new user
 router.post('/signup', registerUser);
-
-// Route for logging a user in
 router.post('/login', authUser);
 
 module.exports = router;
